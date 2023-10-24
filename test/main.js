f398b85fc5d2e11725684f253625a6f456151e3a
falyncarter <141657964+falyncarter@users.noreply.github.com>
2023-10-24 11:14:52 -0700

    redo

--- test/main.js
+++ test/main.js
@@ -40,6 +40,33 @@ const displayContent = (foodLevel, waterLevel, catBehavior, catSummary) => {
 };
 
 
+const renderTblBtn = (cat, index) => {
+  const td = document.createElement("td");
+  const btnEdit = document.createElement("button");
+  btnEdit.textContent = "Edit";
+  btnEdit.addEventListener('click', () => {
+      document.getElementById("foodBowlLvlInput").value = cat.foodLevel;
+      document.getElementById("waterBowlLvlInput").value = cat.waterLevel;
+      document.getElementById("catBehaviorInput").value = cat.catBehavior;
+      document.getElementById("catSummaryInput").value = cat.catSummary;
+      onUpdate(index);
+  });
+
+  const btnDel = document.createElement("button");
+  btnDel.textContent = "Del";
+  btnDel.addEventListener('click', () => {
+      onDelete(index);
+  });
+
+  td.appendChild(btnEdit);
+  td.appendChild(btnDel);
+
+  return td;
+};
+
+
+
+
 
 const updateResults = (content) => {
   const foodResultElement = document.querySelector("#foodResult .results-text");
