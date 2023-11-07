2efb0702c48a41ee678e87e07fc8f8e6ffc4b226
falyncarter <141657964+falyncarter@users.noreply.github.com>
2023-11-07 08:01:16 -0800

    changes

--- test/main.js
+++ test/main.js
@@ -1,113 +1,121 @@
-import { foodResults, waterResults, treatsResults } from './global.js';
+import { foodResults, waterResults, treatsResults } from "./global.js";
 
-const CatCareApp = {
-  resultsArray: [],
-  currentlyEditedIndex: -1,
+const getInputValue = (inputElement) => inputElement ? inputElement.value : null;
 
-  getInputValue: function (selectElement) {
-    return selectElement ? selectElement.value : null;
-  },
+const displayContent = (foodLevel, waterLevel, catBehavior, catSummary) => {
+  const foodContent = foodResults[foodLevel];
+  const waterContent = waterResults[waterLevel];
+  const treatsContent = treatsResults[catBehavior];
 
-  displayContent: function (foodLevel, waterLevel, catBehavior, catSummary) {
-    const foodContent = foodResults[foodLevel];
-    const waterContent = waterResults[waterLevel];
-    const treatsContent = treatsResults[catBehavior];
-
-    if (foodContent && waterContent && treatsContent && catSummary) {
-      return {
-        food: foodContent,
-        water: waterContent,
-        treats: treatsContent,
-        summary: catSummary,
-      };
-    }
-  },
-
-  updateResults: function (content) {
-    // Update the result elements with content
-  },
-
-  addToResultsArray: function (foodLevel, waterLevel, catBehavior, catSummary, content) {
-    const resultObject = {
-      foodLevel,
-      waterLevel,
-      catBehavior,
-      catSummary,
-      content,
+  if (foodContent && waterContent && treatsContent) {
+    return {
+      food: foodContent,
+      water: waterContent,
+      treats: treatsContent,
+      catSummary: catSummary,
     };
-    this.resultsArray.push(resultObject);
-
-    this.displayResultsInConsole();
-  },
-
-  displayResultsInConsole: function () {
-    this.resultsArray.forEach((result, index) => {
-      console.log(`Result ${index + 1}:`, result);
-    });
-  },
-
-  onEditButtonClick: function () {
-    if (this.currentlyEditedIndex !== -1) {
-    
-    }
-  },
-
-  onDeleteButtonClick: function () {
-    if (this.currentlyEditedIndex !== -1) {
-      this.resultsArray.splice(this.currentlyEditedIndex, 1);
-      this.updateResultsDisplay();
-      this.currentlyEditedIndex = -1;
-    }
-  },
-
-  updateResultsDisplay: function () {
-  
-  },
-
-  onSubButtClick: function (event) {
-    event.preventDefault();
-
-    const foodLevel = this.getInputValue(this.foodBowlLvlInput);
-    const waterLevel = this.getInputValue(this.waterBowlLvlInput);
-    const catBehavior = this.getInputValue(this.catBehaviorInput);
-    const catSummary = this.catSummaryInput.value;
-
-    if (foodLevel !== null && waterLevel !== null && catBehavior !== null && catSummary !== "") {
-      const content = this.displayContent(foodLevel, waterLevel, catBehavior, catSummary);
-      if (content) {
-        if (this.currentlyEditedIndex === -1) {
-          this.updateResults(content);
-          this.addToResultsArray(foodLevel, waterLevel, catBehavior, catSummary, content);
-        } else {
-        
-        }
-
-        this.foodBowlLvlInput.value = 'fine';
-        this.waterBowlLvlInput.value = 'fine';
-        this.catBehaviorInput.value = 'great';
-        this.catSummaryInput.value = '';
-      } else {
-        console.log("Content could not be generated.");
-      }
-    } else {
-      this.errorMessageElement.style.display = "block";
-      this.errorMessageElement.textContent = "Please fill in all the required fields.";
-    }
-  },
-
-  init: function () {
-    this.foodBowlLvlInput = document.getElementById("foodBowlLvlInput");
-    this.waterBowlLvlInput = document.getElementById("waterBowlLvlInput");
-    this.catBehaviorInput = document.getElementById("catBehaviorInput");
-    this.catSummaryInput = document.getElementById("catSummaryInput");
-    this.errorMessageElement = document.getElementById("errorMessage");
-
-    const submitBtn = document.getElementById("submitBtn");
-    submitBtn.addEventListener("click", this.onSubButtClick);
-   
-  },
+  }
 };
 
-document.addEventListener("DOMContentLoaded", () => {
-  CatCareApp.init();
-});
+const updateResults = (content) => {
+  const foodResultElement = document.querySelector("#foodResult .results-text");
+  const waterResultElement = document.querySelector("#waterResult .results-text");
+  const treatsResultElement = document.querySelector("#treatsResult .results-text");
+  const catSummaryResultElement = document.querySelector("#catSummaryResult .results-text");
+
+  if (foodResultElement && waterResultElement && treatsResultElement && catSummaryResultElement) {
+    foodResultElement.textContent = content.food;
+    waterResultElement.textContent = content.water;
+    treatsResultElement.textContent = content.treats;
+    catSummaryResultElement.textContent = content.catSummary;
+  }
+};
+
+const resultsArray = [];
+
+const addToResultsArray = (foodLevel, waterLevel, catBehavior, catSummary, content) => {
+  resultsArray.push({
+    foodLevel,
+    waterLevel,
+    catBehavior,
+    catSummary,
+    content,
+  });
+
+  const resultsTable = document.getElementById('resultsTable');
+  if (resultsTable) {
+    // Add rows to the table as before.
+  }
+};
+
+const displayResultsInConsole = () => {
+  resultsArray.forEach((result, index) => {
+    console.log(`Result ${index + 1}:`, result);
+  });
+};
+
+const onSubButtClick = (event) => {
+  event.preventDefault();
+  const foodBowlLvlInput = document.getElementById("foodBowlLvlInput");
+  const waterBowlLvlInput = document.getElementById("waterBowlLvlInput");
+  const catBehaviorInput = document.getElementById("catBehaviorInput");
+  const catSummaryInput = document.getElementById("catSummaryInput");
+
+  const foodLevel = getInputValue(foodBowlLvlInput);
+  const waterLevel = getInputValue(waterBowlLvlInput);
+  const catBehavior = getInputValue(catBehaviorInput);
+  const catSummary = catSummaryInput.value;
+
+  if (foodLevel !== null && waterLevel !== null && catBehavior !== null) {
+    const content = displayContent(foodLevel, waterLevel, catBehavior, catSummary);
+    if (content) {
+      updateResults(content);
+      addToResultsArray(foodLevel, waterLevel, catBehavior, catSummary, content);
+
+      foodBowlLvlInput.value = 'fine';
+      waterBowlLvlInput.value = 'fine';
+      catBehaviorInput.value = 'great';
+      catSummaryInput.value = '';
+
+      const errorMessageElement = document.getElementById("errorMessage");
+      errorMessageElement.style.display = 'none';
+    }
+  } else {
+    const errorMessageElement = document.getElementById("errorMessage");
+    errorMessageElement.style.display = "block";
+    errorMessageElement.textContent = "Please fill in all the required fields.";
+  }
+};
+
+
+const handleDelete = (row, index) => {
+  row.remove();
+  resultsArray.splice(index, 1);
+  displayResultsInConsole();
+};
+
+const handleEditRow = (index) => {
+  const rowToEdit = resultsArray[index];
+  populateFormWithRowData(rowToEdit);
+  resultsArray.splice(index, 1);
+  displayResultsInConsole();
+};
+
+
+const init = () => {
+  const submitBtn = document.getElementById("submitBtn");
+  submitBtn.addEventListener("click", onSubButtClick);
+
+  const editButtons = document.querySelectorAll(".edit-button");
+  const deleteButtons = document.querySelectorAll(".delete-button");
+
+  editButtons.forEach((editButton, index) => {
+    editButton.addEventListener("click", () => handleEditRow(index));
+  });
+
+  deleteButtons.forEach((deleteButton, index) => {
+    deleteButton.addEventListener("click", () => handleDeleteRow(index));
+  });
+};
+
+document.addEventListener("DOMContentLoaded", init);
